6e5f65a4b8962f0879cc321271dfaa1d3dfb1d8a
Nguyen Duc Huy <dhuy.ngn@gmail.com>
2020-06-28 12:14:03 +0700

    Revert "Revert "minor changes""
    This reverts commit 7b62a25460b90a345b541c9765fb04dd7f999902.

--- lib/Authenticate/authenticate.dart
+++ lib/Authenticate/authenticate.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:fooddeliveryapp/Authenticate/register.dart';
 import 'package:fooddeliveryapp/Authenticate/sign_in.dart';
 
 class Authenticate extends StatefulWidget {
   _AuthenticateState createState() => _AuthenticateState();
 }
 
-class _AuthenticateState extends State<Authenticate>
-{
+class _AuthenticateState extends State<Authenticate> {
+  bool showSignIn = true;
+  void toggleView() {
+    setState(() => showSignIn = !showSignIn);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SignIn(),
-    );
+    if (showSignIn) {
+      return SignIn(toggleView: toggleView);
+    }
+    else {
+      return Register(toggleView: toggleView);
+    }
   }
 }
